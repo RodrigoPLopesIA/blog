0b2dff3d699ece7bd9a4d71a0a5921269871623e
RodrigoPLopesIA <rodrigoplopes25@gmail.com>
2025-08-27 21:31:02 -0300

    feat: create metadata to slug post page

--- src/app/post/[slug]/page.tsx
+++ src/app/post/[slug]/page.tsx
@@ -1,4 +1,5 @@
 import { Post } from "@/models/post/post-model";
+import { notFound } from "next/navigation";
 import React from "react";
 
 type Props = {
@@ -6,11 +7,25 @@ type Props = {
     slug: string;
   };
 };
+
+export async function generateMetadata({ params }: Props) {
+  const { slug } = params;
+  const response = await fetch(`http://localhost:3000/api/posts/${slug}`);
+  const post: Post = await response.json();
+  return { title: post.title, description: post.excerpt };
+}
+
 async function PostSlugPage({ params }: Props) {
   const { slug } = params;
 
-  const response = await fetch(`http://localhost:3000/api/posts/${slug}`);
-  const post: Post = await response.json();
+  let post: Post;
+  try {
+    const response = await fetch(`http://localhost:3000/api/posts/${slug}`);
+    post = await response.json();
+  } catch {
+    notFound();
+  }
+
   return (
     <div>
       <h1>{post.title}</h1>
