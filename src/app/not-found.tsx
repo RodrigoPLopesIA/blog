fd6b123e03f134defc7f6c794117db788a44f8f5
RodrigoPLopesIA <rodrigoplopes25@gmail.com>
2025-08-27 21:37:27 -0300

    feat: create loading spinner to post page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -4,23 +4,35 @@ import React from "react";
 
 function NotFound() {
   return (
-    <div className="px-4 flex flex-col md:flex-row items-center gap-2">
-      <div className="flex flex-col max-w-2xl gap-4">
-        <h1 className="text-3xl text-left font-bold text-slate-100 mb-4">
-          Página não encontrada
-        </h1>
-        <p className="text-lg text-slate-200 mb-6 max-w-lg">
-          A página que você procura não existe. Volte para a página principal e
-          confira os novos posts! Clique no link abaixo para voltar.
-        </p>
-        <Link href="/" className="text-white font-medium hover:text-green-500">
-          Voltar para a página principal
-        </Link>
+    <>
+      <title>Pagina não encontrada!</title>
+      <div className="px-4 flex flex-col md:flex-row items-center gap-2">
+        <div className="flex flex-col max-w-2xl gap-4">
+          <h1 className="text-3xl text-left font-bold text-slate-100 mb-4">
+            Página não encontrada
+          </h1>
+          <p className="text-lg text-slate-200 mb-6 max-w-lg">
+            A página que você procura não existe. Volte para a página principal
+            e confira os novos posts! Clique no link abaixo para voltar.
+          </p>
+          <Link
+            href="/"
+            className="text-white font-medium hover:text-green-500"
+          >
+            Voltar para a página principal
+          </Link>
+        </div>
+        <div className="h-full w-full hidden md:block mt-10">
+          <Image
+            src={"/notfound.png"}
+            width={720}
+            height={720}
+            alt="notfound"
+            className="object-cover object-center"
+          />
+        </div>
       </div>
-      <div className="h-full w-full hidden md:block mt-10">
-        <Image src={"/notfound.png"} width={720} height={720} alt="notfound" className="object-cover object-center"/>
-      </div>
-    </div>
+    </>
   );
 }
 
---
+++ src/app/post/[slug]/loading.tsx
@@ -0,0 +1,10 @@
+import SpinLoader from '@/components/SpinLoader/Index'
+import React from 'react'
+
+function loading() {
+  return (
+    <SpinLoader />
+  )
+}
+
+export default loading
